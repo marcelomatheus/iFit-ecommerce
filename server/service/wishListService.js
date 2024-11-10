179a0e3a31e42e35265f3602888d50ff2be44341
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2024-11-09 22:50:05 -0300

    feat: orderService implements

---
+++ server/service/wishListService.js
@@ -0,0 +1,19 @@
+import OrderItemModel from '../models/OrderItemsModel'
+
+const getWishList = async (req,res) => {
+
+}
+
+const insertProductWishList = async (req,res) => {
+
+}
+
+const deleteProductWishList = async (req,res) => {
+
+}
+
+const updateWishList = async (req,res) =>{
+
+}
+
+export default {getWishList, insertProductWishList, deleteProductWishList, updateWishList}
